aa8c73c832168c7af26f120f3951d143e060e855
wengwh <415554484@qq.com>
2018-04-18 16:43:57 +0800

    gitee

--- html/work-admin/app/scripts/admin-constants.js
+++ html/work-admin/app/scripts/admin-constants.js
@@ -8,7 +8,7 @@
 
   angular.module('adminApp')
     .constant('contextRoot', {
-      formService:'http://119.29.96.227/form-service',
+      formService:'http://work.plumdo.com/form-service',
       identityService:'http://work.plumdo.com/identity-service',
       flowService:'http://work.plumdo.com/flow-service'
     })
@@ -23,10 +23,10 @@
       idmRoles: '/roles',
       idmMenus: '/menus',
       formDesgin : function(modelId) {
-        return 'http://119.29.96.227/form-modeler/#/design?modelId=' + modelId;
+        return 'http://work.plumdo.com/form-modeler/#/design?modelId=' + modelId;
       },
       formPreview : function(modelId) {
-        return 'http://119.29.96.227/form-modeler/#/watch?modelId=' + modelId;
+        return 'http://work.plumdo.com/form-modeler/#/watch?modelId=' + modelId;
       },
       flowDesign : function(modelId) {
         return 'http://work.plumdo.com/flow-modeler/#/editor/' + modelId;
